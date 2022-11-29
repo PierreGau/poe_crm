1bad4131f321e3d206e59f63d9cd98a58953f74e
PierreGau <p.gauthier09@gmail.com>
2022-11-29 12:21:16 +0100

    initialisation du projet

---
+++ src/main/java/com/example/demo/model/Client.java
@@ -0,0 +1,156 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="clients")
+public class Client 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="company_name")
+	private String compagnyName;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="phone")
+	private String phone;
+	
+	@Column(name="address")
+	private String address;
+	
+	@Column(name="zip_code")
+	private String zipCode;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="country")
+	private String country;
+	
+	@Column(name="state")
+	private boolean state;
+
+	public Client() {
+	}
+
+	public Client(String compagnyName, String firstName, String lastName, String email, String phone, String address,
+			String zipCode, String city, String country, boolean state) {
+		this.compagnyName = compagnyName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+		this.zipCode = zipCode;
+		this.city = city;
+		this.country = country;
+		this.state = state;
+	}
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCompagnyName() {
+		return compagnyName;
+	}
+
+	public void setCompagnyName(String compagnyName) {
+		this.compagnyName = compagnyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Client [id=%s, compagnyName=%s, firstName=%s, lastName=%s, email=%s, phone=%s, address=%s, zipCode=%s, city=%s, country=%s, state=%s]",
+				id, compagnyName, firstName, lastName, email, phone, address, zipCode, city, country, state);
+	}
+}
